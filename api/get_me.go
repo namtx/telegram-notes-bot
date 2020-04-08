1909069fec05b0052357b3ac31f1e454333cef71
Tran Xuan Nam <nam@nimblehq.co>
2020-04-08 11:18:22 +0700

    Add getMe endpoint

---
+++ api/get_me.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	client := http.Client{}
+	botToken := os.Getenv("TELE_BOT_TOKEN")
+	request, err := http.NewRequest("GET", fmt.Sprintf("https://api.telegram.org/bot%s/getMe", botToken), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, string(body))
+}
