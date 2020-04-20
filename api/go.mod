71ed08e7b90041b83f5976ea5451103b34fb3d72
Tran Xuan Nam <nam@nimblehq.co>
2020-04-20 16:52:55 +0700

    Add Gist integration

---
+++ api/go.mod
@@ -0,0 +1,12 @@
+module github.com/namtx/telegram-notes-bot
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.0 // indirect
+	github.com/google/go-github/v31 v31.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+)
--- api/webhook.go
+++ api/webhook.go
@@ -1,18 +1,53 @@
 package handler
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+const (
+	GIST_ID       = "634e8f12e2f7069cbb71ac4fd5aa4472"
+	DATA_FILENAME = "data.json"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	requestBody, err := ioutil.ReadAll(r.Body)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GH_OAUTH_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	gist, _, err := client.Gists.Get(ctx, GIST_ID)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(requestBody)
 
-	fmt.Fprintf(w, string(requestBody))
+	dataFile := gist.Files[DATA_FILENAME]
+
+	newContent := *dataFile.Content
+
+	update, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newContent = newContent[:len(newContent)-1] + "," + string(update) + "]"
+
+	*dataFile.Content = newContent
+
+	newGist, _, err := client.Gists.Edit(ctx, GIST_ID, gist)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, *newGist.ID)
+
+	w.WriteHeader(http.StatusOK)
 }
